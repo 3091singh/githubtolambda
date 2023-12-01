caa2db883996ef7bbab2c9e3505f18316b27e994
3091singh <149274649+3091singh@users.noreply.github.com>
2023-11-30 22:15:45 -0600

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,13 @@
+import os
+import boto3
 import pandas as pd
 
 def lambda_handler(event, context):
-    # Assuming the CSV file content is in the 'Records' field of the S3 event
-    s3_event = event['Records'][0]['s3']
-    bucket_name = s3_event['bucket']['name']
-    object_key = s3_event['object']['key']
+    # Get the S3 bucket and object key from the event
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
 
-    # Use Boto3 to download the CSV file from S3
-    import boto3
+    # Download the file from S3
     s3 = boto3.client('s3')
     response = s3.get_object(Bucket=bucket_name, Key=object_key)
     csv_content = response['Body'].read().decode('utf-8')
@@ -15,6 +15,6 @@ def lambda_handler(event, context):
     # Use Pandas to read the CSV content into a DataFrame
     df = pd.read_csv(pd.compat.StringIO(csv_content))
 
-    # Now you can use df as you would in your original lambda_handler function
+    # Now you can use df as needed
     print(df)
-    print('Done x1')
+    print('Done processing the file!')
