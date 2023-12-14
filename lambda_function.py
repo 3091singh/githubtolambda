5041ccb183c8d4e9d7ba4175a0eb28d53182aecd
3091singh <149274649+3091singh@users.noreply.github.com>
2023-12-13 18:04:23 -0600

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,21 +4,10 @@ import csv
 import io
 
 def lambda_handler(event, context):
-    s3_client = boto3.client('s3')
+    # Process CSV data using Pandas
+    df = pd.read_csv('deployment_package/test.csv')
 
-    for record in event['Records']:
-        # Extract bucket and key from S3 event record
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
-
-        # Use the extracted bucket and key in get_object
-        response = s3_client.get_object(Bucket=bucket, Key=key)
-        content = response['Body'].read().decode('utf-8')
-
-        # Process CSV data using Pandas
-        df = pd.read_csv(io.StringIO(content))
-
-        # Add your data analysis logic here
+    # Add your data analysis logic here
 
     return {
         'statusCode': 200,
